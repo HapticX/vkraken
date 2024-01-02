385499e99277358e09109eda3236968dd76283ee
Ethosa <social.ethosa@gmail.com>
2024-01-02 13:05:23 +0700

    upd

--- README.md
+++ README.md
@@ -3,7 +3,17 @@
 ![VKrakenIcon](icon.png)
 
 # VKraken
+### VK API for Nim 🔥
 
 </div>
 
+## Features
+- Fully asynchronously
+- Focused on objects
+- Any VK method available
 
+## Install
+Via GitHub:
+```shell
+nimble install https://github.com/HapticX/vkraken
+```
--- tests/test1.nim
+++ tests/test1.nim
@@ -8,16 +8,13 @@ let
   user = initVk(VK_USER_TOKEN)
 
 
-echo "get ..."
+echo "users get ..."
 for user in waitFor bot.users.get(@[1, 2, 556962840]):
   echo user.first_name, " ", user.last_name
-
-echo "search ..."
+echo "users search ..."
 for user in waitFor user.users.search("Фомин Никита", count = 1):
   echo "[", user.id, "] ", user.first_name, " ", user.last_name
 
-echo "get by ID"
-echo (waitFor user~users.get(user_ids = "556962840"))[0]["first_name"]
 
 echo "search hints ..."
 for obj in waitFor user.search.getHints("Никита", limit = 5):
@@ -25,3 +22,8 @@ for obj in waitFor user.search.getHints("Никита", limit = 5):
     echo "GROUP [", $obj.group.id, "] ", obj.group.name
   else:
     echo "USER [", $obj.profile.id, "] ", obj.profile.first_name, " ", obj.profile.last_name
+
+
+# ---=== `~` macro ===--- #
+echo "get by ID"
+echo (waitFor user~users.get(user_ids = "556962840"))[0]["first_name"]
